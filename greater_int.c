58e5811a2cfa5dbbfd709d3c3e880b5fcd9cca57
RidhimaSharmaprofile1 <oasisluminary1101@gmail.com>
2026-09-06 13:17:38 +0530

    Add files via upload

---
+++ greater_int.c
@@ -0,0 +1,23 @@
+// #include <stdio.h>
+// int main()
+// {
+//   int x;
+//   printf("enter the first number:");
+//   scanf("%d",&x);
+//   int y;
+//   printf("enter the second number:");
+//   scanf("%d",&y);
+//   int z;
+//   printf("enter the third number:");
+//   scanf("%d",&z);
+//   if (x>=y && x>=z){
+//     printf("the greatest number is : %d",x);
+//   }
+//   else if (y>=x && y>=z){
+//     printf("the greatest number is : %d",y);
+//   }
+//   else {
+//     printf("the greatest number is : %d",z);
+//   }
+//   return 0;
+// }
---
+++ half_of_no.c
@@ -0,0 +1,11 @@
+// #include <stdio.h>
+// int main()
+// {
+//   int x;
+//   printf("enter the number:");
+//   scanf("%d",&x);
+//   float y=(float)x;
+//   printf("%f",y/2);
+//   return 0;
+  
+// }
