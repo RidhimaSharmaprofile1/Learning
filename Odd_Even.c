e974e76d1b6e0a0557d2d9c07c6860ecc0607df2
RidhimaSharmaprofile1 <oasisluminary1101@gmail.com>
2026-09-07 01:06:57 +0530

    Add files via upload

---
+++ Odd_Even.c
@@ -0,0 +1,17 @@
+// #include<stdio.h>
+// #include<math.h>
+
+// int main()
+// {
+//   int x;
+//   printf("enter the first number:");
+//   scanf("%d",&x);
+//   if(x%2==0)
+//   {
+//     printf("the number is even");
+//   }
+//   else {
+//     printf("the number is odd");
+//   }
+//   return 0;
+// }
