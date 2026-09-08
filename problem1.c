edf92b5e77edbf454357daaf72ed3ba956e2c78c
RidhimaSharmaprofile1 <oasisluminary1101@gmail.com>
2026-09-08 13:21:32 +0530

    Add files via upload

---
+++ problem1.c
@@ -0,0 +1,14 @@
+// #include<stdio.h>
+// int main()
+// {
+//   float x;
+//   printf("enter the number:");
+//   scanf("%f",&x);
+//   if (x== (int)x){
+//     printf("the number is integer");
+//   }
+//   else{
+//     printf("the number is not integer");
+//   }
+//   return 0;
+// }
---
+++ roots_of_quadic_eq (1).c
@@ -0,0 +1,30 @@
+// #include <math.h>
+// #include <stdio.h>
+
+// int main() {
+//   float a, b, c;
+//   printf("Enter the first number (a): ");
+//   scanf("%f", &a);
+
+//   printf("Enter the second number (b): ");
+//   scanf("%f", &b);
+
+//   printf("Enter the third number (c): ");
+//   scanf("%f", &c);
+
+//   float d = pow(b, 2) - 4 * a * c;
+//   printf("Discriminant: %f\n", d);
+
+//   if (d > 0) {
+//     float r1 = (-b + sqrt(d)) / (2 * a);
+//     float r2 = (-b - sqrt(d)) / (2 * a);
+//     printf("Roots are: %f and %f\n", r1, r2);
+//   } else if (d == 0) {
+//     float r1 = -b / (2 * a);
+//     printf("Roots are: %f and %f (same root)\n", r1, r1);
+//   } else {
+//     printf("Roots are imaginary\n");
+//   }
+
+//   return 0;
+// }
