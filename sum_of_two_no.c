074726c7f2dec2f5c3c4bf27dcd91defdcc5dc70
RidhimaSharmaprofile1 <oasisluminary1101@gmail.com>
2026-09-10 20:30:37 +0530

    Add files via upload

---
+++ sum_of_two_no.c
@@ -0,0 +1,20 @@
+// #include<stdio.h>
+// int main()
+// {
+//   float x;
+//   printf("enter the first number:");
+//   scanf("%f",&x);
+//   float y;
+//   printf("enter the second number:");
+//   scanf("%f",&y);
+//   float z=x+y;
+//   float mul=x*y;
+//   float div=x/y;
+//   float sub=x-y;
+//   printf("sum of two numbers is : %f\n",z);
+//   printf("multiplication of two numbers is : %f\n",mul);
+//   printf("division of two numbers is : %f\n",div);
+//   printf("subtraction of two numbers is :%f\n",sub);
+  
+//   return 0;
+// }
