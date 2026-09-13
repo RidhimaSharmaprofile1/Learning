0ccfdc12bcd60d94874d83d85003701212f6ae5c
RidhimaSharmaprofile1 <oasisluminary1101@gmail.com>
2026-09-13 23:36:30 +0530

    Add files via upload

---
+++ basic.c
@@ -0,0 +1,13 @@
+// #include<stdio.h>
+// int main()
+// {
+//   int x;
+//   printf("enter the number:");
+//   scanf("%d",&x);
+//   for(int i=1;i<=x;i++){
+//     printf("hello World\n");
+  
+    
+//   }
+//   return 0;
+// }
